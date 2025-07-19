a1a4ce435700478d76b75529d4cb74a0e4728685
kbhatt <kbhatt@extremenetworks.com>
2025-07-19 12:12:35 +0530

    Did 7 problems. Total count is now 12

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void swap(int[] nums, int i, int j) {
@@ -132,7 +130,138 @@ public static void sort_colors()
         }
 
     }
+    public static int find_the_dup() {
+        int[] v = {1,3,4,2,2};
+        Arrays.sort(v);
+        for(int i= 1;i<v.length;i++)
+        {
+            if(v[i] == v[i-1])
+            {
+                return v[i];
+            }
+        }
+        return -1;
+    }
+    public static void single_number() {
+        int[] v = {4,1,2,1,2};
+        int result = 0;
+        for(int i = 0;i<v.length;i++)
+        {
+            result ^= v[i];
+        }
+        System.out.println(result);
+    }
+    public static void maxi_sub()
+    {
+        int [] v = {5,4,-1,7,8};
+        int current_max = v[0];
+        int max_so_far = v[0];
+        for(int i=1;i<v.length;i++)
+        {
+            current_max = Math.max(v[i],current_max+v[i]);
+            max_so_far = Math.max(max_so_far,current_max);
+        }
+        System.out.println(max_so_far);
+    }
+    public static void three_sum()
+    {
+        int [] v = {-1,0,1,2,-1,-4};
+        Arrays.sort(v);
+
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i= 0;i<v.length-2;i++)
+        {
+            if(i>0 && v[i] == v[i-1])
+                continue;
+            int left = i+1,right= v.length-1;
+            while(left<right)
+            {
+                int current_sum = v[i] + v[left] + v[right];
+                if(current_sum ==0 )
+                {
+                    ans.add(new ArrayList<>(Arrays.asList(v[i],v[left],v[right])));
+                    while(left<right && v[left] == v[left+1])
+                    {
+                        left++;
+                    }
+                    while (right>left && v[right] == v[right-1])
+                    {
+                        right--;
+                    }
+                    left+=1;
+                    right-=1;
+                }
+                 else if (current_sum < 0)
+                    {
+                        left+=1;
+                    }
+                    else
+                    {
+                        right-=1;
+                    }
+            }
+
+
+        }
+        System.out.println(ans.toString());
+    }
+    public static int[] two_sum()
+    {
+        int [] v ={3,2,4};
+        int target = 6;
+        int[] ans = new int[2];
+        Map<Integer,Integer>m =  new HashMap<>();
+        for(int i =0 ;i <v.length;i++)
+        {
+
+            if(m.containsKey( target - v[i]))
+            {
+                ans[0] = m.get( target - v[i]);
+                ans[1] = i;
+                return ans;
+            }
+            m.put(v[i],i);
+        }
+        return ans;
+    }
+    public static void group_ana()
+    {
+        String [] s = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        Map<String,List<String>> map = new HashMap<>();
+        for(String word : s)
+        {
+            char [] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String sortedWord = new String (ch);
+            if(!map.containsKey(sortedWord))
+            {
+                map.put(sortedWord,new ArrayList<>());
+
+            }
+            map.get(sortedWord).add(word);
+        }
+        List<List<String>> ans = new ArrayList<>();
+        System.out.println(map.values());
+        for(List<String> groups : map.values())
+        {
+            ans.add(groups);
+        }
+
+//        List<List<String>> ans = new ArrayList<>();
+//        Map<String,List<String>> map = Arrays.stream(s).collect(Collectors.groupingBy(
+//                p -> {
+//                    char [] ch = p.toCharArray();
+//                    Arrays.sort(ch);
+//                    return new String(ch);
+//                }
+//        ));
+//        for(Map.Entry<String,List<String>> entry : map.entrySet())
+//        {
+//            ans.add(entry.getValue());
+//        }
+        
+    }
     public static void main(String[] args) {
-    prod_ex_self();
+    group_ana();
     }
 }
